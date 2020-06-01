ce1176a56a639170fa665dc89433284515f1f350
Jafett Puga <pjafett1999@gmail.com>
2020-06-01 14:40:28 -0500

    Integrate user login, register, and logout

--- bug_tracker/bug_tracker/urls.py
+++ bug_tracker/bug_tracker/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from bugtrack import views
+from register import views as reg_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
+    path('register/', reg_view.register, name='register'),
+    path('', include('django.contrib.auth.urls')),
 ]
