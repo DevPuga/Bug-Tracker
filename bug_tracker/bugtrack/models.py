6722a0dd465c8d31436768e7777780714e8cbb63
Jafett Puga <pjafett1999@gmail.com>
2020-05-31 19:12:47 -0500

    Created BugTicket model

--- bug_tracker/bugtrack/models.py
+++ bug_tracker/bugtrack/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
-class project(models.Model):
+class Project(models.Model):
 	project_name = models.CharField(max_length = 30)
 	project_description = models.TextField()
-	
+
+class BugTicket(models.Model):
+	STATUS_CHOICES = [
+		('O' : 'Open'),
+		('IP' : 'In-Progress'),
+		('C' : 'Closed'),
+	]
+
+	bug_title = models.CharField(max_length = 30)
+	bug_description = models.TextField()
+	status = models.CharField(max_length = 2, choices = STATUS_CHOICES, default = O)
+	date_created = models.DateField()
+	project = models.ForeignKey('Project')
